007364a5d08be371928b3d0175f4fe28e62d908d
Keegan Fontaine <keeganjf1@gmail.com>
2023-03-09 06:52:39 -0500

    Finished most of the carousel styles

--- js/script.js
+++ js/script.js
@@ -4,3 +4,26 @@ const hamBtn = document.querySelector(".primary-header__ham");
 hamBtn.addEventListener("click", () => {
   header.classList.toggle("active");
 });
+
+function moveCards() {
+  const cards = document.querySelectorAll(".card");
+
+  cards.forEach((card) => {
+    card.dataset.order = parseInt(card.dataset.order) + 1;
+
+    console.log(card.dataset.order);
+
+    if (card.dataset.order > cards.length) {
+      setTimeout(() => {
+        card.style.opacity = 0;
+        card.dataset.order = 1;
+
+        setTimeout(() => {
+          card.style.opacity = 1;
+        }, 1000);
+      }, 200);
+    }
+  });
+}
+
+setInterval(moveCards, 7500);
